22326609018a8a01f718bc20598810332ae579ff
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-10-18 12:25:00 -0700

    Py3 Cleanup

--- authorizer.py
+++ authorizer.py
@@ -1,17 +1,21 @@
-
-from flask import Flask, request, Response
-import scitokens
-import ConfigParser
 import argparse
-import traceback
-import re
-import logging
+import configparser
+import errno
 import json
+import logging
+import re
+import subprocess
+import traceback
+
+import scitokens
+from flask import Flask, request, Response
+
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 g_authorized_issuers = {}
 g_global_audience = ""
+
 
 @app.route('/auth')
 def flask_listener():
@@ -23,7 +27,7 @@ def flask_listener():
         op = 'read'
     elif orig_op in ["PUT", "POST", "DELETE", "MKCOL", "COPY", "MOVE"]:
         op = 'write'
-    
+
     # Look at the path as well
     orig_path = request.headers.get('X-Original-URI')
     
@@ -41,7 +45,8 @@ def flask_listener():
         token = scitokens.SciToken.deserialize(raw_token, audience=g_global_audience)
     except Exception as e:
         resp = Response("No Authorization header")
-        resp.headers['WWW-Authenticate'] = 'Bearer realm="scitokens",error="invalid_token",error_description="{0}"'.format(str(e))
+        resp.headers['WWW-Authenticate'] = \
+            'Bearer realm="scitokens",error="invalid_token",error_description="{0}"'.format(str(e))
         logging.exception("Failed to deserialize SciToken")
         traceback.print_exc()
         return resp, 401
@@ -53,7 +58,8 @@ def flask_listener():
         return message, 200
     else:
         if 'jti' in token._claims:
-            logging.error("Failed to authenticate SciToken ID {0} because {1}".format(token['jti'], message))
+            logging.error("Failed to authenticate SciToken ID {0} because {1}".format(token['jti'],
+                                                                                      message))
         else:
             logging.error("Failed to authenticate SciToken because {0}".format(message))
         return message, 403
@@ -67,7 +73,7 @@ def test_operation_path(op, path, token):
     """
     # Setup a SciToken Enforcer
     if token['iss'] not in g_authorized_issuers:
-        return (False, "Issuer not in configuration")
+        return False, "Issuer not in configuration"
     issuer_url = token['iss']
     issuer = g_authorized_issuers[issuer_url]
     base_path = issuer['base_path']
@@ -75,38 +81,35 @@ def test_operation_path(op, path, token):
     # The path above should consist of"
     # $base_path + / + $auth_path + / + $request_path = path
     if not path.startswith(base_path):
-        print "Requested path does not start with base_path"
-        return (False, "The requested path does not start with the base path")
+        logging.error("Requested path does not start with base_path")
+        return False, "The requested path does not start with the base path"
     
     # Now remove the base path so we just get the auth_path + request_path
     filepath_on_disk = path.replace(base_path, "", 1)
 
     if issuer['use_impersonation']:
-        if impersonation_check(token, filepath_on_disk):
-            return (True, "")
+        if impersonation_test(token, op, filepath_on_disk):
+            return True, ""
         # Note: Fall through to next authorizer
 
     enforcer = scitokens.scitokens.Enforcer(token['iss'], audience=g_global_audience)
     try:
         if enforcer.test(token, op, filepath_on_disk):
-            return (True, "")
+            return True, ""
         else:
-            return (False, "Path not allowed")
+            return False, "Path not allowed"
     except scitokens.scitokens.EnforcementError as e:
-        print e
-        return (False, str(e))
-
-    # Fallthrough - We should never be here
-    return (False, "[ERROR] We should not be here.")
+        print(e)
+        return False, str(e)
 
 
 # From xrootd-scitokens, we want the same configuration
 def config(fname):
-    print "Trying to load configuration from %s" % fname
-    cp = ConfigParser.SafeConfigParser()
+    print("Trying to load configuration from %s" % fname)
+    cp = configparser.ConfigParser()
     try:
         with open(fname, "r") as fp:
-            cp.readfp(fp)
+            cp.read_file(fp)
     except IOError as ie:
         if ie.errno == errno.ENOENT:
             return
@@ -115,22 +118,21 @@ def config(fname):
         if not section.lower().startswith("issuer "):
             continue
         if 'issuer' not in cp.options(section):
-            print "Ignoring section %s as it has no `issuer` option set." % section
+            print("Ignoring section %s as it has no `issuer` option set." % section)
             continue
         if 'base_path' not in cp.options(section):
-            print "Ignoring section %s as it has no `base_path` option set." % section
+            print("Ignoring section %s as it has no `base_path` option set." % section)
             continue
         issuer = cp.get(section, 'issuer')
-        use_impersonation = cp.getboolean(section, 'impersonation', fallback=False)
-
         base_path = cp.get(section, 'base_path')
         base_path = scitokens.urltools.normalize_path(base_path)
         issuer_info = g_authorized_issuers.setdefault(issuer, {})
         issuer_info['base_path'] = base_path
-        issuer_info['use_impersonation']  = use_impersonation
+        issuer_info['use_impersonation'] = cp.getboolean(section, 'impersonation', fallback=False)
         if 'map_subject' in cp.options(section):
             issuer_info['map_subject'] = cp.getboolean(section, 'map_subject')
-        print "Configured token access for %s (issuer %s): %s" % (section, issuer, str(issuer_info))
+        print("Configured token access for %s (issuer %s): %s" %
+              (section, issuer, str(issuer_info)))
     
     global g_global_audience
     if 'audience_json' in cp.options("Global"):
@@ -145,7 +147,8 @@ def config(fname):
 
 def impersonation_test(token, op, filepath_on_disk):
     test_option = "-w" if op == "write" else "-r"
-    return_code = subprocess.call(["sudo", "-u", token["sub"], "test", test_option, filepath_on_disk])
+    return_code = subprocess.call(["sudo", "-u", token["sub"],
+                                   "test", test_option, filepath_on_disk])
     return return_code == 0
 
 
@@ -163,8 +166,7 @@ def main():
     
     # Set up listener for events
     app.run(host='localhost', port=1234)
-    
-    pass
+
 
 if __name__ == "__main__":
     main()
